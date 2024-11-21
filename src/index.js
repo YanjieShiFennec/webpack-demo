1cefa5a8fb0eefad417cbf619e1811950c8e2cdd
yshi80 <yshi80@ford.com>
2024-11-21 11:18:39 +0800

    webpack load css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash 现在使用 import 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
